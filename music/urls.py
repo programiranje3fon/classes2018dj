8099b1b4b56b63e877c5219ad1164594de2fbdd2
devedzic <devedzic@gmail.com>
2018-12-14 22:08:54 +0100

    Commit 1 - initial setup (urls.py, music.urls.py, simplest views.index()).

---
+++ music/urls.py
@@ -0,0 +1,11 @@
+"""The URLconfig of the music app."""
+
+
+from django.urls import path
+
+from music import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
+
--- music/views.py
+++ music/views.py
@@ -1,3 +1,11 @@
+# Create your views here.
+
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Hi there :)<h1>')
+
+
