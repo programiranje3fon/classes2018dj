05e8bb05767b37ba31d16efa3ddb896c1c7ce317
devedzic <devedzic@gmail.com>
2018-12-18 09:45:46 +0100

    Commit 3 - Step-by-step development of the index and base templates, function-based index() views and class-based Performer views. Reuse (with modification) of common_content.html and common_sidebar.html. Reuse of static files (images, styles.css).

--- music/urls.py
+++ music/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('', views.index, name='index')
 ]
 
+# urlpatterns += [
+#     path('performers/<int:pk>', views.performer_detail, name='performer-detail')
+# ]
+
 urlpatterns += [
-    path('performers/<int:pk>', views.performer_detail, name='performer-detail')
+    path('performers/<int:pk>', views.PerformerDetailView.as_view, name='performer-detail')
 ]
 
--- music/views.py
+++ music/views.py
@@ -3,16 +3,50 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, CreateView, DeleteView, UpdateView
 
 from music.models import Performer
 
 
+# def index(request):
+#     return HttpResponse('<h1>Hi there :)<h1>')
+
+# def index(request):
+#     n_performers = Performer.objects.all().count()
+#     rng = range(0, 3)
+#     context = {
+#         'n_performers': n_performers,
+#         'range': rng
+#     }
+#     # return render(request, 'index_0.html', context=context)
+#     # return render(request, 'index_1.html', context=context)
+#     return render(request, 'index_2.html', context=context)
+
 def index(request):
-    return HttpResponse('<h1>Hi there :)<h1>')
+    n_performers = Performer.objects.all().count()
+    context = {
+        'n_performers': n_performers,
+    }
+    # return render(request, 'index_0.html', context=context)
+    # return render(request, 'index_1.html', context=context)
+    # return render(request, 'index_2.html', context=context)
+    return render(request, 'index.html', context=context)
 
 
-def performer_detail(request, pk):
-    # return HttpResponse(str(Performer.objects.get(id=pk)))
-    return HttpResponse(str(Performer.objects.get(id=pk).get_absolute_url()))
+# def performer_detail(request, pk):
+#     # return HttpResponse(str(Performer.objects.get(id=pk)))
+#     return HttpResponse(str(Performer.objects.get(id=pk).get_absolute_url()))
+
+class PerformerDetailView(DetailView):
+    model = Performer
+
+
+class PerformerListView(ListView):
+    model = Performer
+    template = 'music/performer_list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Performer.objects.all()
 
 
